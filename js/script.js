939f8a6a0f7b851681a815dbce7fe4b0f8052ee5
Palágyi Réka <reka.bapa@tutanota.com>
2023-07-08 21:04:22 +0200

    Mobilos javítgatások

--- js/script.js
+++ js/script.js
@@ -1,30 +1,45 @@
-const lista2Elemek = document.getElementsByClassName("lista-2");
-const lista1Elem = document.querySelector(".lista-1");
-lista2Elemek[0].style.justifyContent = "left";
-lista2Elemek[1].style.justifyContent = "left";
-lista2Elemek[5].style.justifyContent = "right";
-lista2Elemek[4].style.justifyContent = "right";
+const lista2listak = document.getElementsByClassName("lista-2");
+const lista2elemek = document.querySelectorAll(".lista-2 > li");
+const lista1 = document.querySelector(".lista-1");
+const lista1elemek = document.querySelectorAll(".lista-1 > li");
+lista2listak[0].style.justifyContent = "left";
+lista2listak[1].style.justifyContent = "left";
+lista2listak[5].style.justifyContent = "right";
+lista2listak[4].style.justifyContent = "right";
 
-for (let i = 0; i < lista2Elemek.length; i++) {
-  lista2Elemek[i].style.content = `${i}`;
-}
-
-if (window.innerWidth >= 1083) {
-  lista2Elemek[2].style.justifyContent = "left";
-  lista2Elemek[2].style.paddingLeft = "19.7vw";
-} else {
-  lista2Elemek[2].style.paddingLeft = "0";
-}
-
-const lista1elemek = lista1Elem.querySelectorAll("li");
-for (const elem of lista1elemek) {
+for(const elem of lista1elemek){
   elem.addEventListener("click", fixalj);
+  elem.addEventListener("touchend", fixalj);
+}
+
+document.querySelector("#menu-icon").addEventListener("click", helyreAllit);
+
+function helyreAllit(){
+  lista1.style.left = "0";
+  for (const elem of lista1elemek){
+    elem.style.textDecoration = "none";
+  }
 }
 
 function fixalj(evt) {
-  if (window.innerWidth >= 635) {
-    evt.target.style.visibility = "hidden";
-  } else {
-    evt.target.style.visibility = "hidden";
+  console.log(evt);
+  for (const elem of lista1elemek){
+    elem.style.textDecoration = "none";
   }
+  for(const elem of lista2elemek){
+    elem.style.justifyContent = "center";
+  }
+  if ((window.innerWidth <= 1150 || window.screen.width <= 1150) && (window.innerWidth > 450 || window.screen.width > 450)) {
+      evt.target.style.textDecoration = "underline";
+  }
+  if(window.innerWidth <= 450 || window.screen.width <= 450) {
+    lista1.style.left = "-999px";
+    console.log(evt.target.nextElementSibling);
+    evt.target.nextElementSibling.classList.add("onmobile2");
+   }
+}
+
+const kereseskep = document.getElementById("kereses");
+if(window.innerWidth <= 1150 || window.screen.width <= 1150){
+  kereseskep.src="css/searchiconmt.png";
 }
