7bc9219b0d907f328d7abc357eb4e750c663e0fe
vernyeljanos <136849032+vernyeljanos@users.noreply.github.com>
2023-08-22 20:41:15 +0200

    Post szerkesztés

---
+++ routes/users.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+const passport = require("passport");
+const User = require("../models/user");
+const users = require("../controllers/users");
+
+router
+  .route("/login")
+  .get(users.renderLogin)
+  .post(
+    passport.authenticate("local", {
+      failureFlash: true,
+      failureRedirect: "/login",
+    }),
+    users.login
+  );
+
+router.route("/register").get(users.renderRegister).post(users.register);
+
+router.get("/logout", users.logout);
+
+module.exports = router;
