6075a30073aef99171dc19df0b76252ed9b7e1c3
Palágyi Réka <reka.bapa@tutanota.com>
2024-06-07 20:21:06 +0200

    Responsive New books

--- public/js/boilerplate.js
+++ public/js/boilerplate.js
@@ -1,10 +1,17 @@
 const socmedContainer = document.querySelector("#socmed-container");
-const profileLogout = document.querySelector(".profile-logout");
+const profileLogout = document.querySelector(".profile-logout")
+  ? document.querySelector(".profile-logout")
+  : document.querySelector(".login-signup > div");
 const logoContainer = document.querySelector(".logo-container");
 
 function replaceSocmed() {
   if (window.innerWidth <= 768) {
-    profileLogout.insertBefore(socmedContainer, profileLogout.lastElementChild);
+    if (profileLogout) {
+      profileLogout.insertBefore(
+        socmedContainer,
+        profileLogout.lastElementChild
+      );
+    }
   } else {
     logoContainer.insertBefore(socmedContainer, logoContainer.lastElementChild);
   }
--- public/js/bookAll.js
+++ public/js/bookAll.js
@@ -1,3 +1,7 @@
+const gep =
+  window.innerWidth > 1200 &&
+  window.screen.width > 1200 &&
+  window.outerWidth > 1200;
 const bookts = document.querySelectorAll(".booktxt");
 bookts.forEach((booktxt) => {
   let booktext = booktxt.getAttribute("data-b");
@@ -12,13 +16,15 @@ bookts.forEach((booktxt) => {
 });
 
 document.querySelectorAll(".bookw").forEach((b) => {
-  b.style.width = `${b.querySelector(".booklbl img").offsetWidth + 30}px`;
-  const ad = b.querySelector(".bookad")
-    ? b.querySelector(".bookad").offsetHeight
-    : 0;
-  b.style.height = `${
-    b.querySelector(".booklbl img").offsetHeight + ad + 30
-  }px`;
+  if (gep) {
+    b.style.width = `${b.querySelector(".booklbl img").offsetWidth + 30}px`;
+    const ad = b.querySelector(".bookad")
+      ? b.querySelector(".bookad").offsetHeight
+      : 0;
+    b.style.height = `${
+      b.querySelector(".booklbl img").offsetHeight + ad + 30
+    }px`;
+  }
 });
 
 document.querySelectorAll(".bookpop").forEach((p) => {
@@ -46,9 +52,11 @@ function togglePopup(p, input, txt, lbl, img) {
     p.classList.add("book-popup");
     lbl.style.display = "none";
     txt.insertBefore(img, txt.firstChild);
+    document.querySelector("body").classList.add("no-scroll");
   } else {
     p.classList.remove("book-popup");
     img.remove();
+    document.querySelector("body").classList.remove("no-scroll");
     lbl.style.display = "block";
   }
 }
